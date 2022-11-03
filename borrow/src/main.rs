a1350d7dc2c9977e3dfab9a5c47b2d40cb2f3053
erkelost <1256029807@qq.com>
2022-11-03 18:30:52 +0800

    chore: borrow 借用与引用

--- borrow/src/main.rs
+++ borrow/src/main.rs
@@ -7,14 +7,38 @@
 
 // 下面是如何定义并使用一个（新的）calculate_length 函数，它以一个对象的引用作为参数而不是获取值的所有权：
 // 这些 & 符号就是 引用，它们允许你使用值但不获取其所有权。
+// fn main() {
+//     let s1 = String::from("hello");
+
+//     let len = calculate_length(&s1);
+
+//     println!("The length of '{}' is {}.", s1, len);
+// }
+
+// fn calculate_length(s: &String) -> usize {
+//     s.len()
+// }
+// fn calculate_length(s: &String) -> usize {
+//     // s 是对 String 的引用
+//     s.len()
+// } // 这里，s 离开了作用域。但因为它并不拥有引用值的所有权，
+//   // 所以什么也不会发生
+
+// fn main() {
+//     let s1 = String::from("hello");
+//     let res: usize = test(&s1);
+//     println!("{} {}", s1, res);
+// }
+
+// fn test(s: &String) -> usize {
+//     s.len()
+// }
+//   不能尝试修改 借用的变量 引用就是借用
+
 fn main() {
     let s1 = String::from("hello");
-
-    let len = calculate_length(&s1);
-
-    println!("The length of '{}' is {}.", s1, len);
+    test(&s1);
 }
-
-fn calculate_length(s: &String) -> usize {
-    s.len()
+fn test(s: &String) {
+    s.push_str("", "world");
 }
