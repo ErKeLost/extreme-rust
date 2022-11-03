0d0d698f1a9e584946ba3d4695f622e62f4ae97e
erkelost <1256029807@qq.com>
2022-11-03 14:10:30 +0800

    chore: 认识所有权

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -33,9 +33,97 @@
 // rust 禁止使用无效引用无效
 
 // 复制
+// fn main() {
+//     let s1 = String::from("hello");
+//     let s2 = s1.clone();
+
+//     println!("s1 = {}, s2 = {}", s1, s2);
+// }
+// fn main() {
+//     let s = String::from("hello"); // s 进入作用域
+
+//     takes_ownership(s); // s 的值移动到函数里 ...
+//                         // ... 所以到这里不再有效
+//     // println!("{}", s);
+//     let x = "asdasd"; // x 进入作用域
+
+//     makes_copy(x); // x 应该移动函数里，
+//     println!("{}", x);
+//     // 但 i32 是 Copy 的，所以在后面可继续使用 x
+// } // 这里, x 先移出了作用域，然后是 s。但因为 s 的值已被移走，
+//   // 所以不会有特殊操作
+
+// fn takes_ownership(some_string: String) {
+//     // some_string 进入作用域
+//     println!("{}", some_string);
+// } // 这里，some_string 移出作用域并调用 `drop` 方法。占用的内存被释放
+
+// fn makes_copy(some_integer: &str) {
+//     // some_integer 进入作用域
+//     println!("{}", some_integer);
+// } // 这里，some_integer 移出作用域。不会有特殊操作
+
+// 返回值 与 作用域
+
+// fn main() {
+//     let s1 = String::from("hello world");
+//     let s2 = gives();
+//     println!("{}", s2);
+//     let s3 = takes_and_gives_back(s1);
+//     println!("{}", s3);
+//     println!("{}", s1);
+// }
+
+// fn gives() -> String {
+//     let str = String::from("owner");
+//     str
+// }
+// // takes_and_gives_back 将传入字符串并返回该值
+// fn takes_and_gives_back(a_string: String) -> String {
+//     // a_string 进入作用域
+
+//     a_string // 返回 a_string 并移出给调用的函数
+// }
+
+// // 解析
+// fn main() {
+//     let s1 = gives_ownership(); // gives_ownership 将返回值
+//                                 // 移给 s1
+
+//     let s2 = String::from("hello"); // s2 进入作用域
+
+//     let s3 = takes_and_gives_back(s2); // s2 被移动到
+//                                        // takes_and_gives_back 中,
+//                                        // 它也将返回值移给 s3
+// } // 这里, s3 移出作用域并被丢弃。s2 也移出作用域，但已被移走，
+//   // 所以什么也不会发生。s1 移出作用域并被丢弃
+
+// fn gives_ownership() -> String {
+//     // gives_ownership 将返回值移动给
+//     // 调用它的函数
+
+//     let some_string = String::from("yours"); // some_string 进入作用域
+
+//     some_string // 返回 some_string 并移出给调用的函数
+// }
+
+// // takes_and_gives_back 将传入字符串并返回该值
+// fn takes_and_gives_back(a_string: String) -> String {
+//     // a_string 进入作用域
+
+//     a_string // 返回 a_string 并移出给调用的函数
+// }
+
 fn main() {
     let s1 = String::from("hello");
-    let s2 = s1.clone();
 
-    println!("s1 = {}, s2 = {}", s1, s2);
+    let (s2, len) = calculate_length(s1);
+
+    println!("The length of '{}' is {}.", s2, len);
+}
+
+fn calculate_length(s: String) -> (String, usize) {
+    let length = s.len(); // len() 返回字符串的长度
+
+    (s, length)
 }
