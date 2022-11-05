8397f09ad5659258b2f6a370d68ddc3fb9e64c4c
erkelost <1256029807@qq.com>
2022-11-05 15:39:18 +0800

    chore: dbg! 宏 测试

--- struct-start/src/main.rs
+++ struct-start/src/main.rs
@@ -138,22 +138,57 @@
 
 // 使用结构体重构 赋予更多的意义
 
+// struct Rectangle {
+//     width: u32,
+//     height: u32,
+// }
+
+// fn main() {
+//     let react = Rectangle {
+//         width: 30,
+//         height: 600,
+//     };
+//     println!(
+//         "The area of the rectangle is {} square pixels.",
+//         area(&react)
+//     );
+//     println!("{}", react.height);
+// }
+// fn area(rectangle: &Rectangle) -> u32 {
+//     rectangle.width * rectangle.height
+// }
+
+// 调试结构体派生 trait
+// 示例 5-12：增加属性来派生 Debug trait，并使用调试格式打印 Rectangle 实例s
+// #[derive(Debug)]
+// struct Rectangle {
+//     width: u32,
+//     height: u32,
+// }
+
+// fn main() {
+//     let rect1 = Rectangle {
+//         width: 30,
+//         height: 50,
+//     };
+
+//     // println!("rect1 is {:?}", rect1);
+//     println!("rect1 is {:#?}", rect1);
+//     dbg!(&rect1);
+// }
+
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
 }
 
 fn main() {
-    let react = Rectangle {
-        width: 30,
-        height: 60,
+    let scale = 2;
+    let rect1 = Rectangle {
+        width: dbg!(30 * scale),
+        height: 50,
     };
-    println!(
-        "The area of the rectangle is {} square pixels.",
-        area(&react)
-    );
-    println!("{}", react.height);
-}
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
+
+    dbg!(&rect1);
 }
