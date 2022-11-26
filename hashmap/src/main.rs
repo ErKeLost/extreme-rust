16d664047bfdd0ef655d6bf370d475c86865f92d
erkelost <1256029807@qq.com>
2022-11-26 15:47:03 +0800

    chore: start hashmap

--- hashmap/src/main.rs
+++ hashmap/src/main.rs
@@ -1,3 +1,9 @@
+#![allow(unused)]
 fn main() {
-    println!("Hello, world!");
+    use std::collections::HashMap;
+
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
 }
