7540577379c954f03be3d33213e0a360e33ba9d3
erkelost <1256029807@qq.com>
2022-12-30 16:04:25 +0800

    style: file struct

--- fun/src/main.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-    println!("Hello, world!");
-    main_another()
-}
-fn main_another() {
-    println!("Hello, main_another!");
-}
--- hashmap/src/main.rs
+++
@@ -1,9 +0,0 @@
-#![allow(unused)]
-fn main() {
-    use std::collections::HashMap;
-
-    let mut scores = HashMap::new();
-
-    scores.insert(String::from("Blue"), 10);
-    scores.insert(String::from("Yellow"), 50);
-}
