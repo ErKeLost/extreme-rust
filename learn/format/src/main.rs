ac9c11b3d3b5d916a962db9ce2ffc7ecac10a514
ERKELSOT <1256029807@qq.com>
2023-03-13 20:16:38 +0800

    chore: format

--- learn/format/src/main.rs
+++ learn/format/src/main.rs
@@ -1,5 +1,4 @@
 fn main() {
-    println!("Hello, world!");
     println!("Hello"); // => "Hello"
     println!("Hello, {}!", "world"); // => "Hello, world!"
     println!("The number is {}", 1); // => "The number is 1"
@@ -8,3 +7,9 @@ fn main() {
     println!("{} {}", 1, 2); // => "1 2"
     println!("{:04}", 42); // => "0042" with leading zeros
 }
+
+// println 宏接收的是一个可变参数，
+//  第一个参数是一个字符串常量,
+// 他表示最终输出到终端的字符串格式 包含 {} 符号是占位符
+// ，用来表示后面的参数的值
+// 会被 println后面的参数移除替换
