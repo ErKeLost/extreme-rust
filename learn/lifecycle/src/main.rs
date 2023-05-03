b22c430ebf325455ff39e5cb298369629c9f7fb4
erkelost <1256029807@qq.com>
2023-05-03 12:49:31 +0800

    chore: lifecycle and test

--- learn/lifecycle/src/main.rs
+++ learn/lifecycle/src/main.rs
@@ -78,5 +78,27 @@
 
 
 fn main() {
+    // let r;
+    // {
+    //     let x = 5;
+    //     r = &x;
+    // }
+    // println!("r: {}", r);
     
+
+    let string1 = String::from("long string is long");
+    {
+        let string2 = String::from("xyz");
+        let result = longest(string1.as_str(), string2.as_str());
+        println!("the longest string is {}", result);
+    }
+}
+
+
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len(){
+        x
+    }else{
+        y
+    }
 }
